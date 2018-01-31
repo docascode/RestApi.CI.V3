a1e896a0d6cd4af1304a0b3145f2292315a96610
Yannan Wang <yannw@microsoft.com>
2018-01-31 17:47:04 +0800

    Add UnitTest for Security and SeeAlso

---
+++ Microsoft.RestApi.UnitTest/SeeAlso.UnitTest.cs
@@ -0,0 +1,34 @@
+﻿namespace Microsoft.RestApi.UnitTest
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Microsoft.RestApi.Common;
+    using Microsoft.RestApi.Models;
+    using Microsoft.RestApi.Transformers;
+
+    using Xunit;
+
+    [Trait("Owner", "yannw@microsoft.com")]
+    [Trait("Priority", "1")]
+    [Trait("Category", "OperationEntity")]
+    public class SeeAlsoUnitTest : BaseUnitTest
+    {
+        [Fact]
+        public void OperationEntity_GetSeeAlso()
+        {
+            var openApiDocument = LoadOpenApiDocument("../../samples/GetSeeAlso.yaml");
+            var operation = openApiDocument.Paths.Values.First().Operations.Values.First();
+
+            var seeAlso = RestOperationTransformer.TransformExternalDocs(operation);
+            Assert.NotNull(seeAlso);
+            Assert.Equal(3, seeAlso.Count);
+            Assert.Equal("Find more info here", seeAlso[0].Description);
+            Assert.Equal("description A", seeAlso[1].Description);
+            Assert.Equal("description B", seeAlso[2].Description);
+            Assert.Equal("https://example.com/", seeAlso[0].Url);
+            Assert.Equal("https://exampleA.com", seeAlso[1].Url);
+            Assert.Equal("https://exampleB.com", seeAlso[2].Url);
+        }
+    }
+}
